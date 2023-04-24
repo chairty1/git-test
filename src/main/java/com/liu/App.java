8aeb9df29b3663106085d6dad12e4c2db5a1a347
Test1 <chairty133@gmail.com>
2023-04-24 11:29:36 +0800

    master commit

--- src/main/java/com/liu/App.java
+++ src/main/java/com/liu/App.java
@@ -12,5 +12,6 @@ public class App
         System.out.println( "Hello git!" );
         System.out.println( "Hello git1!" );
         System.out.println( "Hello git2!" );
+        System.out.println( "maset merge" );
     }
 }
