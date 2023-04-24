f15222044185f212d6a17ccd87b7641ac855a08f
chairty1 <102907100+chairty1@users.noreply.github.com>
2023-04-24 11:39:50 +0800

    Update App.java

--- src/main/java/com/liu/App.java
+++ src/main/java/com/liu/App.java
@@ -15,5 +15,6 @@ public class App
         System.out.println( "maset merge" );
         System.out.println( "hot-fix meger" );
         System.out.println( "push test" );
+        System.out.println( "pull test" );
     }
 }
